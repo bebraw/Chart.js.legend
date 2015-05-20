e98a27299031729d3d74e82250214423936e3ba9
Juho Vepsalainen <bebraw@gmail.com>
2015-05-20 09:40:35 +0300

    Add doughnut example
    Related to #13.

--- src/demo.js
+++ src/demo.js
@@ -4,6 +4,7 @@ else window.attachEvent('onload', main);
 function main() {
     lineChart();
     pieChart();
+    doughnutChart();
 }
 
 function lineChart() {
@@ -61,4 +62,29 @@ function pieChart() {
     var pieChart = new Chart(ctx).Pie(data);
 
     legend(document.getElementById("pieLegend"), data, pieChart);
+}
+
+function doughnutChart() {
+    var data = [
+        {
+            value: 40,
+            color:"#F38630",
+            label: 'Animals'
+        },
+        {
+            value : 20,
+            color : "#E0E4CC",
+            label: 'People'
+        },
+        {
+            value : 30,
+            color : "#69D2E7",
+            label: 'Aliens'
+        }
+    ];
+
+    var ctx = document.getElementById("doughnutChart").getContext("2d");
+    var doughnutChart = new Chart(ctx).Doughnut(data);
+
+    legend(document.getElementById("doughnutLegend"), data, doughnutChart);
 }
